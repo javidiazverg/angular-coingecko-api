3f3aaa1a3dcb6cdc2f60475781379244b0886100
javidiazverg <javidiazvergara@gmail.com>
2022-02-10 18:43:55 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24h: number;
+  market_cap_rank: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'coingecko-api';
+export class AppComponent implements OnInit{
+
+  coins: Coin[] = [];
+  filteredCoins: Coin[] = [];
+  titles: string [] = [
+    '#  ',
+    'Coin Name',
+    'Price $',
+    'Price Change',
+    'Market Rank',
+  ]
+  searchText = '';
+
+  constructor(private http: HttpClient) {}
+
+  searchCoin() {
+    this.filteredCoins = this.coins.filter(coin => 
+      coin.name.toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase()) ||
+      coin.symbol.toLocaleLowerCase().includes(this.searchText.toLocaleLowerCase())
+      );
+  }
+
+  ngOnInit() {
+      this.http.get<Coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+        .subscribe(
+          (res) => {
+            console.log(res);
+            this.coins = res;
+            this.filteredCoins = res;
+          },
+          (err) => console.log(err))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
